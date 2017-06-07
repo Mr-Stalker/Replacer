7060fe987c0154945d59aab80dad4f2aa71a9f44
Vitor Ribeiro <vitordex@hotmail.com>
2017-06-06 23:13:51 -0300

    Correção de ao invés de realocar a função adicionar à um novo.

--- Replacer/Form1.Designer.cs
+++ Replacer/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -125,16 +125,15 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button2
+            // Button3
             // 
-            this.button2.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(71, 251);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(165, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "\"Historical Quotes\" Pattern";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button1_Click);
+            this.Button3.Location = new System.Drawing.Point(62, 245);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(165, 23);
+            this.Button3.TabIndex = 8;
+            this.Button3.Text = "\"Historical Quotes\" Pattern";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Form1
             // 
@@ -142,8 +141,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(304, 311);
+            this.Controls.Add(this.Button3);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
@@ -171,7 +170,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Button3;
     }
 }
 
--- Replacer/Form1.cs
+++ Replacer/Form1.cs
@@ -14,11 +14,11 @@ namespace Replacer
         {
             InitializeComponent();
 
-            /*comboBox1.Items.AddRange(new string[] {
-                ReplaceWhere.Before.ToString(),
-                ReplaceWhere.Replace.ToString(),
-                ReplaceWhere.After.ToString()
-            });*/
+            comboBox1.Items.AddRange(new string[] {
+                "Before",
+                "Replace",
+                "After"
+            });
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -28,22 +28,53 @@ namespace Replacer
                     textBox1.Text,
                     openFileDialog1.FileName.Substring(0, openFileDialog1.FileName.LastIndexOf('\\') + 1),
                     openFileDialog1.SafeFileName,
-                    //comboBox1.SelectedIndex == -1 ? (ReplaceWhere)2 : (ReplaceWhere)comboBox1.SelectedIndex,
+                    comboBox1.SelectedIndex == -1 ? 2 : comboBox1.SelectedIndex,
                     checkBox1.Checked
                 );
         }
 
-        public void ReadAndReplace(string word, string filePath, string fileName, /*ReplaceWhere where,*/ bool overrideFile)
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                ReadAndReplacePattern(
+                    textBox1.Text,
+                    openFileDialog1.FileName.Substring(0, openFileDialog1.FileName.LastIndexOf('\\') + 1),
+                    openFileDialog1.SafeFileName,
+                    checkBox1.Checked
+                );
+        }
+
+        private void ReadAndReplace(string word, string filePath, string fileName, int where, bool overrideFile)
+        {
+            try
+            {
+                var fileText = "";
+
+                using (var reader = new StreamReader(filePath + fileName))
+                    fileText = reader.ReadToEnd().Replace(word, (where == 2 ? word : "") + ";" + (where == 0 ? word : ""));
+
+                using (var writer = new StreamWriter(filePath + (overrideFile ? "" : "out") + fileName))
+                    writer.Write(fileText);
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("O arquivo não foi encontrado", "Erro");
+                return;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Houve um erro, tente novamente", "Erro");
+                return;
+            }
+
+            MessageBox.Show("Processo completado", "Sucesso");
+        }
+
+        private void ReadAndReplacePattern(string word, string filePath, string fileName, bool overrideFile)
         {
             try
             {
                 var fileText = new string[0];
-
-                /*using (var reader = new StreamReader(filePath + fileName))
-                    fileText = reader.ReadToEnd().Replace(word,
-                        (where == ReplaceWhere.After ? word : "") +
-                        ";" +
-                        (where == ReplaceWhere.Before ? word : ""));*/
 
                 using (var reader = new StreamReader(filePath + fileName))
                 {
@@ -71,11 +102,4 @@ namespace Replacer
             MessageBox.Show("Processo completado", "Sucesso");
         }
     }
-
-    /*public enum ReplaceWhere
-    {
-        Before,
-        Replace,
-        After
-    }*/
 }
